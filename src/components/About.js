4a311fb4edfc661341d83c88e252bfd879a1209f
Joycey <Jcheung8385@gmail.com>
2023-04-02 18:44:24 +0100

    Replace dummy text with real panda info, added grid to make my image and article more responsive

--- src/components/About.js
+++ src/components/About.js
@@ -4,25 +4,30 @@ const About = () => {
       <section className="section" id="about">
         <div className="section__heading">
           <h2>
-            About <span>Pandas</span>
+            About the <span> Giant Panda</span>
           </h2>
         </div>
         <div className="section-center about-center">
-          <div className="about-img">
-            <img src="" className="about-photo" alt="pandas" />
+          <div className="about__img">
+            <img
+              src="https://static.businessinsider.com/image/51dd6b0ceab8eaa223000013/image.jpg"
+              className="about__photo"
+              alt="pandas"
+            />
           </div>
-          <article className="about-info">
-            <h3>10 fun facts</h3>
+          <article className="about__info">
+            <h3 className="about__subheading">overview</h3>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
-              sapiente odio unde dolor?
+              The panda, with its distinctive black and white coat, is adored by
+              the world and considered a national treasure in China.
             </p>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
-              sapiente odio unde dolor?
+              Pandas live mainly in temperate forests high in the mountains of
+              southwest China, where they subsist almost entirely on bamboo.
+              They must eat around 26 to 84 pounds of it every day, depending on
+              what part of the bamboo they are eating.
             </p>
+            {/* information taken from WWF */}
             <a href="#home" className="btn">
               read more
             </a>
