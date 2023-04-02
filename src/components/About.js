97c83c0a87512c1144321b3bdd19c8542b8aaa52
Joycey <Jcheung8385@gmail.com>
2023-04-02 17:29:37 +0100

    Added the article section html for the about page. Added dummy text

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,32 @@
 const About = () => {
   return (
     <>
-      <section>
+      <section className="section" id="about">
         <div className="section__heading">
           <h2>
             About <span>Pandas</span>
           </h2>
+        </div>
+        <div className="section-center about-center">
+          <div className="about-img">
+            <img src="" className="about-photo" alt="pandas" />
+          </div>
+          <article className="about-info">
+            <h3>10 fun facts</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
+              sapiente odio unde dolor?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
+              sapiente odio unde dolor?
+            </p>
+            <a href="#home" className="btn">
+              read more
+            </a>
+          </article>
         </div>
       </section>
     </>
