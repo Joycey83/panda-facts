48496d8edcfc1f7526a0acbbfb99225957a61186
Joycey <Jcheung8385@gmail.com>
2023-04-02 11:05:21 +0100

    added the basic flexbox css to my simple navbar, added a image to my navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,30 @@ const Navbar = () => {
     <>
       <nav className="navbar__container">
         <div className="navbar__center">
-          <div className="navbar__header">
-            <img src="" className="nav-logo" alt="Pandas" />
-            <button type="button" className="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-            <ul className="nav-links">
-              <li>
-                <a href="#" className="nav-link">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  Panda Facts
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link">
-                  Photo Gallery
-                </a>
-              </li>
-            </ul>
-          </div>
+          <img
+            src="https://cdn.dribbble.com/users/1642260/screenshots/9165901/panda1_4x.png"
+            width="50"
+            height="50"
+            className="navbar__logo"
+            alt="Pandas"
+          />
+          <ul className="nav-links">
+            <li>
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Panda Facts
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Photo Gallery
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
