6c90f670eb23be54e6f7decef2911e8a18bbd7a0
Joycey <Jcheung8385@gmail.com>
2023-04-04 18:51:40 +0100

    Added 10 panda fact array of object in my data.js . will be used to map my funfact card list in my panda fact component

--- src/data.js
+++ src/data.js
@@ -22,4 +22,65 @@ export const socialLinks = [
   },
 ];
 
-export const pandaFacts = [{ id: 1, img: "" }];
+export const pandaFacts = [
+  {
+    id: 1,
+    img: "",
+    title: "One",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 2,
+    img: "",
+    title: "Two",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 3,
+    img: "",
+    title: "Three",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 4,
+    img: "",
+    title: "Four",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 5,
+    img: "",
+    title: "Five",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 6,
+    img: "",
+    title: "Six",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 7,
+    img: "",
+    title: "Seven",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 8,
+    img: "",
+    title: "Eight",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 9,
+    img: "",
+    title: "Nine",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+  {
+    id: 10,
+    img: "",
+    title: "Ten",
+    text: "Pandas have lived on earth for two to three million years.",
+  },
+];
