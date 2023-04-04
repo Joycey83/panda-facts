b145e04c4aa90e4fba358715d094321b4580a528
Joycey <Jcheung8385@gmail.com>
2023-04-04 19:14:05 +0100

    imported the Heading component inside About component , also added mainHeading and spanHeading prop

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,4 @@
+import Heading from "./Heading";
 const About = () => {
   return (
     <>
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,4 +1,12 @@
-const Heading = () => {
-  return <div>Heading</div>;
+const Heading = ({ mainHeading, spanHeading }) => {
+  return (
+    <>
+      <div className="section__heading">
+        <h2>
+          {mainHeading} <span> {spanHeading}</span>
+        </h2>
+      </div>
+    </>
+  );
 };
 export default Heading;
