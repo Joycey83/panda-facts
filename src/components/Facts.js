dd8c091101b44d7b6c5b7b4954dd0f23842e23a6
Joycey <Jcheung8385@gmail.com>
2023-04-04 19:22:34 +0100

    added the mainheading and spanheading prop for Facts component

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -4,7 +4,7 @@ const Facts = () => {
   return (
     <>
       <section className="section fact__container" id="pandaFacts">
-        <Heading />
+        <Heading mainHeading="10 facts about" spanHeading="Giant Panda" />
         <div className="grid-container">
           <div className="fact__card section-center ">
             <img
