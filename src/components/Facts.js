4505cfd0c04756c167356cb749a72e994c7328af
Joycey <Jcheung8385@gmail.com>
2023-04-06 17:40:05 +0100

    I have fixed the issue on my the CSS grid container did not work, I had to map the items inside the grid-container and then return the card

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -7,7 +7,24 @@ const Facts = () => {
       <section className="section fact__container" id="pandaFacts">
         <Heading mainHeading="10 facts about" spanHeading="Giant Panda" />
 
-        <div className="grid-container"></div>
+        <div className="grid-container">
+          {pandaFacts.map((facts) => {
+            const { id, img, title, text } = facts;
+
+            return (
+              <div className="fact__card section-center">
+                <img className="fact__card--img" src={img} alt="panda" />
+                <div className="card-body">
+                  <h5 className="card-title">{title}</h5>
+                  <p className="card-text">{text}</p>
+                  <a href="#" className="btn">
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </>
   );
