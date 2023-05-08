86c511f3cbc6e960271e4a0b90ef59cd66779c83
Joycey <Jcheung8385@gmail.com>
2023-05-08 15:28:25 +0100

    imported the Heading component into Threats.js component

--- src/components/Threats.js
+++ src/components/Threats.js
@@ -1,8 +1,10 @@
 import HabitatLossFragmentation from "./HabitatLossFragmentation";
+import Heading from "./Heading";
 
 const Threats = () => {
   return (
     <>
+      <Heading mainHeading="major" spanHeading="threats" />
       <HabitatLossFragmentation />
     </>
   );
