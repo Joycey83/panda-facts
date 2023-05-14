894d2638a341ebb4dbf94c529973ed343ebd6da5
Joycey <Jcheung8385@gmail.com>
2023-05-14 17:52:15 +0100

    added sub heading for habitat loss component

--- src/components/HabitatLossFragmentation.js
+++ src/components/HabitatLossFragmentation.js
@@ -3,6 +3,9 @@ const HabitatLossFragmentation = ({ title, img, textOne, textTwo }) => {
     <>
       <section className="section habitat-loss--frag">
         <div className="habitat-loss__container">
+          <h2 className="habitat-loss--heading">
+            Habitat loss and fragmentation
+          </h2>
           <img src={img} alt={title} />
         </div>
       </section>
