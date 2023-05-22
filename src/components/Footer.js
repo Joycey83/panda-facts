1500c2343b00b4379bb3b7d852fc3c414f01b6a8
Joycey <Jcheung8385@gmail.com>
2023-05-22 17:45:43 +0100

    mapped the social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
+import { socialMediaLinks } from "../data";
+import SocialLinks from "./SocialLinks";
+
 const Footer = () => {
   return (
     <>
-      <footer className="footer__container"></footer>
+      <footer className="footer__container">
+        <ul className="social--icons">
+          {socialMediaLinks.map((link) => {
+            return (
+              <SocialLinks key={link.id} {...link} itemclass="social--icon" />
+            );
+          })}
+        </ul>
+      </footer>
     </>
   );
 };
